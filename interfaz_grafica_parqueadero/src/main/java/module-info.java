c75cc416bf03f603c8a508d3022125198e8c8a21
JuanDaM01 <159975691+JuanDaM01@users.noreply.github.com>
2024-05-14 22:24:41 -0500

    Delete interfaz_grafica_parqueadero directory

--- interfaz_grafica_parqueadero/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module co.edu.uniquindio.poo.interfaz_grafica_parqueadero {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens co.edu.uniquindio.poo.interfaz_grafica_parqueadero to javafx.fxml;
-    exports co.edu.uniquindio.poo.interfaz_grafica_parqueadero;
-}
